e562f6d1a4e0de5942133547e26eff6460bc3198
SarV <sarv@Ravis-MacBook-Pro-2.local>
2026-04-26 09:56:00 -0400

    chore: configure base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  base: '/3D-Laser-Lab/',
   server: {
     https: true,
     host: '0.0.0.0',
